0dbcb86c9156a83a1818e61ea18ac3e8e737b362
afonya2 <adamhir@freemail.hu>
2025-08-28 22:41:33 +0200

    added the remaining admin commands

--- src/modules/adminCommands.lua
+++ src/modules/adminCommands.lua
@@ -8,7 +8,8 @@ local helpText = [[Available commands:
 - `apiban <user> <reason>` - API ban a user
 - `unapiban <user>` - Unban a user from the API
 - `allocate <item> (money/item) <delta-value>` - Allocate or deallocate money or items to/from an item, internal value for money
-- `balance <user>` - Check a user's balance
+- `setItemLimit <item> <limit>` - Set the limit for an item
+- `balance/bal <user>` - Check a user's balance
 - `changebal <user> <delta-value>` - Change a user's balance, internal value
 ]]
 
@@ -142,6 +143,67 @@ local function adminCommands(user, args, data, userData)
                 chatbox.tell(user, "&cItem not found.", config.shopname, "format") 
             end
         end
+    elseif args[2] == "setItemLimit" then
+        if #args < 4 then
+            chatbox.tell(user, "&cPlease specify an item and a limit.", config.shopname, "format")
+            return
+        end
+        local limit = tonumber(args[4])
+        if (limit == nan) or (math.floor(limit) ~= limit) or (limit == 0) then
+            chatbox.tell(user, "&cPlease specify a valid value.", config.shopname, "format")
+            return
+        end
+        local possible, item, cat, pos = utils.queryItem(args[3])
+        if item then
+            SolidityPools.items[cat][pos].itemLimit = limit
+            utils.saveCategory(cat, SolidityPools.items[cat])
+            chatbox.tell(user, "&aSuccessfully changed item limit of &7"..item.name.."&a to &7"..limit.."&a.", config.shopname, "format")
+        else
+            local bestMatch = nil
+            local bestPerc = 0
+            for k, v in pairs(possible) do
+                if v > bestPerc then
+                    bestPerc = v
+                    bestMatch = k
+                end
+            end
+            if bestPerc > 50 then
+                chatbox.tell(user, "&cItem not found. &aDid you mean: &7" .. bestMatch .. "&a?", config.shopname, "format")
+            else
+                chatbox.tell(user, "&cItem not found.", config.shopname, "format") 
+            end
+        end
+    elseif (args[2] == "balance") or (args[2] == "bal") then
+        if #args < 3 then
+            chatbox.tell(user, "&cPlease specify a user.", config.shopname, "format")
+            return
+        end
+        local targetUser = args[3]
+        local targetData, targetUUID = utils.findUserByUUIDOrName(targetUser)
+        if not targetData then
+            chatbox.tell(user, "&cUser not found.", config.shopname, "format")
+            return
+        end
+        chatbox.tell(user, "&7"..targetData.name.." ("..targetUUID..")&a's balance: &6" .. (targetData.balance/1000000) .. "kro", config.shopname, "format")
+    elseif args[2] == "changebal" then
+        if #args < 4 then
+            chatbox.tell(user, "&cPlease specify a user and a delta-value.", config.shopname, "format")
+            return
+        end
+        local amount = tonumber(args[4])
+        if (amount == nan) or (math.floor(amount) ~= amount) or (amount == 0) then
+            chatbox.tell(user, "&cPlease specify a valid value.", config.shopname, "format")
+            return
+        end
+        local targetUser = args[3]
+        local targetData, targetUUID = utils.findUserByUUIDOrName(targetUser)
+        if not targetData then
+            chatbox.tell(user, "&cUser not found.", config.shopname, "format")
+            return
+        end
+        targetData.balance = targetData.balance + amount
+        utils.saveUser(targetUUID, targetData)
+        chatbox.tell(user, "&aSuccessfully changed &7"..targetData.name.."&a's balance to &6" .. (targetData.balance/1000000) .. "kro", config.shopname, "format")
     else
         chatbox.tell(user, "&cUnknown command. &aType &7\\"..config.command.." admin help &afor a list of commands.", config.shopname, "format")
     end
